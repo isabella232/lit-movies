744fb0150aa9a80ec26b84f959cce4014bb4de05
Addy Osmani <addyosmani@gmail.com>
2022-01-15 10:56:34 -0800

    [docs] add minor rollup comments

--- rollup.config.js
+++ rollup.config.js
@@ -7,17 +7,23 @@ import summary from "rollup-plugin-summary";
 
 export default {
   plugins: [
+    // Entry point for application build; can specify a glob to build multiple HTML files for non-SPA app
     html({
       input: 'index.html',
     }),
+    // Resolve bare module specifiers to relative paths
     resolve(),
+    // Minify HTML template literals
     minifyHTML(),
+    // Minify JS
     terser({
       ecma: 2021,
       module: true,
       warnings: true,
     }),
+    // Print bundle summary
     summary(),
+    // Optional: copy any static assets to build directory
     copy({
       patterns: ['src/img/**/*', 'src/locales/**/*']
     })
